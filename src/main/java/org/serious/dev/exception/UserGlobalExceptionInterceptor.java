b52ade979c4a7fd05bc89b6b9207ec41ba2d8bec
serious-deeev <seriousdeeev@gmail.com>
2025-06-15 23:35:07 +0500

    feat: добавление логирования запросов и ответов

--- src/main/java/org/serious/dev/exception/UserGlobalExceptionInterceptor.java
+++ src/main/java/org/serious/dev/exception/UserGlobalExceptionInterceptor.java
@@ -1,17 +1,29 @@
 package org.serious.dev.exception;
 
+import io.grpc.ForwardingServerCall;
 import io.grpc.ForwardingServerCallListener;
 import io.grpc.Metadata;
 import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
 import io.grpc.Status;
+import lombok.extern.slf4j.Slf4j;
+import org.serious.dev.grpc.UserRequest;
+import org.serious.dev.grpc.UserResponse;
 import org.springframework.stereotype.Component;
 
+import java.net.SocketAddress;
 import java.util.Map;
+import java.util.UUID;
 
+import static io.grpc.Grpc.TRANSPORT_ATTR_REMOTE_ADDR;
+
+@Slf4j
 @Component
 public class UserGlobalExceptionInterceptor implements ServerInterceptor {
+
+    private static final Metadata.Key<String> X_REQUEST_ID_HEADER =
+            Metadata.Key.of("x-request-id", Metadata.ASCII_STRING_MARSHALLER);
 
     private static final Map<Class<? extends Exception>, Status> EXCEPTION_STATUS_MAP = Map.of(
             NoSuchUserException.class, Status.NOT_FOUND
@@ -23,15 +35,33 @@ public class UserGlobalExceptionInterceptor implements ServerInterceptor {
             Metadata headers,
             ServerCallHandler<ReqT, RespT> next
     ) {
+        String requestId = getRequestId(headers);
+        SocketAddress clientAddress = call.getAttributes().get(TRANSPORT_ATTR_REMOTE_ADDR);
+        String incomingRpc = call.getMethodDescriptor().getFullMethodName();
 
-        ServerCall.Listener<ReqT> delegate = next.startCall(call, headers);
+        ServerCall<ReqT, RespT> serverCall = new ForwardingServerCall.SimpleForwardingServerCall<>(call) {
+            @Override
+            public void sendMessage(RespT responseMessage) {
+                logResponse(clientAddress, requestId, responseMessage);
+                super.sendMessage(responseMessage);
+            }
+        };
+
+        ServerCall.Listener<ReqT> delegate = next.startCall(serverCall, headers);
 
         return new ForwardingServerCallListener.SimpleForwardingServerCallListener<>(delegate) {
+            @Override
+            public void onMessage(ReqT requestMessage) {
+                logRequest(clientAddress, incomingRpc, requestId, requestMessage);
+                super.onMessage(requestMessage);
+            }
+
             @Override
             public void onHalfClose() {
                 try {
                     super.onHalfClose();
                 } catch (Exception exception) {
+                    logError(requestId, exception);
                     handleEndpointException(exception, call);
                 }
             }
@@ -46,5 +76,36 @@ public class UserGlobalExceptionInterceptor implements ServerInterceptor {
                         .withDescription(exception.getMessage()),
                 new Metadata()
         );
+    }
+
+    private String getRequestId(Metadata headers) {
+        String requestId = headers.get(X_REQUEST_ID_HEADER);
+        if (requestId == null || requestId.isBlank()) {
+            requestId = UUID.randomUUID().toString();
+        }
+        return requestId;
+    }
+
+    private <ReqT> void logRequest(SocketAddress clientAddress, String incomingRpc, String requestId, ReqT requestMessage) {
+        UserRequest request = (UserRequest) requestMessage;
+        log.info("[{}] получен grpc-запрос → client: {}, rpcCall: {}, id пользователя: {}",
+                requestId,
+                clientAddress,
+                incomingRpc,
+                request.getId());
+    }
+
+    private <RespT> void logResponse(SocketAddress clientAddress, String requestId, RespT responseMessage) {
+        UserResponse response = (UserResponse) responseMessage;
+        log.info("[{}] найден пользователь с id = {}, отправляем ответ client: {}",
+                requestId,
+                response.getId(),
+                clientAddress);
+    }
+
+    private void logError(String requestId, Exception exception) {
+        log.error("[{}] при обработке запроса возникла ошибка: {}",
+                requestId,
+                exception.getMessage());
     }
 }
